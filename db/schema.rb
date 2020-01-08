21191a7412c7052ebc8d00fdd89f82a93a0351b9
Yohei Higashi <yoohe0722@gmail.com>
2020-01-08 12:35:39 +0900

    Logモデルを削除しPostモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_134743) do
+ActiveRecord::Schema.define(version: 2020_01_08_033307) do
 
-  create_table "logs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "text"
     t.text "image"
