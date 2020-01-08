345b4b84e9806ef2c830f8488e5939a832f0a4d2
Yohei Higashi <yoohe0722@gmail.com>
2020-01-08 13:27:27 +0900

    投稿機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts' => 'posts#index'
+  get  'posts'      =>  'posts#index'
+  get  'posts/new'  =>  'posts#new'
+  post 'posts'      =>  'posts#create'
 end
