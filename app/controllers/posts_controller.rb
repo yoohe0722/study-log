a51ad3ef0d0c1d0d03b5db0035758133bf40ed23
Yohei Higashi <yoohe0722@gmail.com>
2020-01-08 17:30:54 +0900

    削除機能、編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,22 @@ class PostsController < ApplicationController
     Post.create(image: post_params[:image], text: post_params[:text], user_id: current_user.id)
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy if post.user_id == current_user.id
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.user_id == current_user.id
+      post.update(post_params)
+    end
+  end
+
   private
     def post_params
       params.permit(:image, :text)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root  'posts#index'
-  get   'posts'       =>  'posts#index'
-  get   'posts/new'   =>  'posts#new'
-  post  'posts'       =>  'posts#create'
-  get   'users/:id'   =>  'users#show'
+  root    'posts#index'
+  get     'posts'       =>  'posts#index'
+  get     'posts/new'   =>  'posts#new'
+  post    'posts'       =>  'posts#create'
+  delete  'posts/:id'   =>  'posts#destroy'
+  get     'posts/:id/edit'  => 'posts#edit'
+  patch   'posts/:id'  => 'posts#update'
+  get     'users/:id'   =>  'users#show'
 end
