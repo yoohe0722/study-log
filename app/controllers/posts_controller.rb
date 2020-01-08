3f39a09bd2dd36098c0f0c9b4d82589c5b7773da
Yohei Higashi <yoohe0722@gmail.com>
2020-01-08 14:29:48 +0900

    ユーザーのログイン機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
     @posts = Post.order("id DESC").page(params[:page]).per(5)
   end
@@ -13,5 +15,9 @@ class PostsController < ApplicationController
   private
     def post_params
       params.permit(:name, :image, :text)
+    end
+
+    def move_to_index
+      rredirect_to action: :index unless user_signed_in?
     end
 end
