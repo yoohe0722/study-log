7e35c6a321b10afb3be3bfaa5bf6830a6191fac8
Yohei Higashi <yoohe0722@gmail.com>
2020-01-10 19:59:51 +0900

    date,study_timeのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_10_104759) do
+ActiveRecord::Schema.define(version: 2020_01_10_105916) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_01_10_104759) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+    t.date "date"
+    t.integer "study_time"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
