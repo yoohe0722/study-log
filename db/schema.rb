380a6bc15c48e936afdeba5a97c96d578e248603
Yohei Higashi <yoohe0722@gmail.com>
2020-01-10 19:49:34 +0900

    imageカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_093925) do
+ActiveRecord::Schema.define(version: 2020_01_10_104759) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2020_01_08_093925) do
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "text"
-    t.text "image"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
