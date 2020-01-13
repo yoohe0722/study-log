45b515586b0ffcf875f7978c2f1d9249833a93ac
Yohei Higashi <yoohe0722@gmail.com>
2020-01-13 15:47:58 +0900

    インスタンス変数countの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @nickname = @user.nickname
     @posts = @user.posts.page(params[:page]).per(5).order("created_at DESC")
-    @study_time_all = @posts.sum(:study_time)
-    @study_time_all_hour = @study_time_all/60.round(1)
-    @study_time_month =@posts.by_month(field: :date).sum(:study_time)
-    @study_time_month_hour = @study_time_month/60.round(1)
-    @study_time_week =@posts.by_week(field: :date, startday: :monday).sum(:study_time)
-    @study_time_week_hour = @study_time_week/60.round(1)
+    @count_all = @posts.sum(:study_time)
+    @count_all_hour = @count_all/60.round(1)
+    @count_month =@posts.by_month(field: :date).sum(:study_time)
+    @count_month_hour = @count_month/60.round(1)
+    @count_week =@posts.by_week(field: :date, startday: :monday).sum(:study_time)
+    @count_week_hour = @count_week/60.round(1)
   end
 end
