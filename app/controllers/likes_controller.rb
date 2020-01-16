a8de4f6382a42671f6db6c15f6b34876114c4d8a
Yohei Higashi <yoohe0722@gmail.com>
2020-01-17 01:25:30 +0900

    respond_toでjson形式の指定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,16 +3,24 @@ class LikesController < ApplicationController
 
   def create
     Like.create(post_id: params[:post_id], user_id: current_user.id)
+    respond_to do |format|
+      format.html { redirect_to post_path(params[:post_id])  }
+      format.json
+    end
   end
 
   def destroy
     unlike = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     unlike.destroy if unlike.user_id == current_user.id
+    respond_to do |format|
+      format.html { redirect_to post_path(params[:post_id])  }
+      format.json
+    end
   end
 
   def set_variables
-    params.permit(:post_id, :user_id)
-    @id_name = "#like-link-#{params[:post_id]}"
+    @post = Post.find(params[:post_id])
+    @id_name = "#like-link-#{@post.id}"
   end
 
   def likes_params
