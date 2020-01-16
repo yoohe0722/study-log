e81615ce6ebd8e3f9e407437196787920ac3488b
Yohei Higashi <yoohe0722@gmail.com>
2020-01-16 15:49:51 +0900

    likesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_094753) do
+ActiveRecord::Schema.define(version: 2020_01_15_122546) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_01_15_094753) do
     t.date "date"
     t.integer "study_time"
     t.integer "comments_count", default: 0, null: false
+    t.integer "likes_count", default: 0, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
