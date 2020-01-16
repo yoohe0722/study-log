d4a0cbe05038ca547048a8d698cda4b080aa6b4a
Yohei Higashi <yoohe0722@gmail.com>
2020-01-16 15:49:29 +0900

    postモデルのアソシエーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comment
+  has_many :comments
   has_many :likes, dependent: :destroy
-  has_many :liking_users, through: :likes, source: :user
 end
