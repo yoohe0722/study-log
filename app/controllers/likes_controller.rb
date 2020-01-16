4878d89b18017ce9e02f4c919c2ab18b70e994f4
Yohei Higashi <yoohe0722@gmail.com>
2020-01-16 17:24:54 +0900

    destroyアクションの修正
    find→find_byでエラー修正できた

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,21 @@
 class LikesController < ApplicationController
+  before_action :set_variables
 
   def create
     Like.create(post_id: params[:post_id], user_id: current_user.id)
   end
 
   def destroy
-    unlike = Like.find(post_id: params[:post_id],user_id: current_user.id )
+    unlike = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     unlike.destroy if unlike.user_id == current_user.id
+  end
+
+  def set_variables
+    params.permit(:post_id, :user_id)
+    @id_name = "#like-link-#{params[:post_id]}"
+  end
+
+  def likes_params
+    params.permit(:post_id, :user_id)
   end
 end
