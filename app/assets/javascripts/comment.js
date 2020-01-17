5b34bfdc079d12c08fb209bb47472340e8464b10
Yohei Higashi <yoohe0722@gmail.com>
2020-01-17 18:19:52 +0900

    コメントが入力されていない場合はalertが出るように変更

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -11,24 +11,29 @@ $(function(){
   }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.comments').append(html);
-      $('.textbox').val('');
-      $('.form__submit').prop('disabled', false);
-    })
-    .fail(function(){
-      alert('error');
-    })
-  })
+    if ($("textarea[name='text']").val() == '') {
+      alert('コメントを入力してください');
+      return false;
+    } else {
+        var formData = new FormData(this);
+        var url = $(this).attr('action')
+        $.ajax({
+          url: url,
+          type: "POST",
+          data: formData,
+          dataType: 'json',
+          processData: false,
+          contentType: false
+        })
+        .done(function(data){
+          var html = buildHTML(data);
+          $('.comments').append(html);
+          $('.textbox').val('');
+          $('.form__submit').prop('disabled', false);
+        })
+        .fail(function(){
+          alert('error');
+        })
+    }
+  });
 });
