5c6ad9f65d7d3b829495af4fd0abfcd84cbcb72e
Yohei Higashi <yoohe0722@gmail.com>
2020-01-21 20:12:00 +0900

    countメソッドの設定で記述簡略化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,19 @@ class UsersController < ApplicationController
     @nickname = @user.nickname
     @posts = @user.posts.page(params[:page]).per(5).order("created_at DESC")
     @count_all = @posts.sum(:study_time)
-    @count_all_hour = sprintf("%.1f",@count_all/60.to_f)
-    @count_month =@posts.by_month(field: :date).sum(:study_time)
-    @count_month_hour = sprintf("%.1f",@count_month/60.to_f)
-    @count_week =@posts.by_week(field: :date, startday: :monday).sum(:study_time)
-    @count_week_hour = sprintf("%.1f",@count_week/60.to_f)
+    @count_all_hour = hour(@count_all)
+    @count_month = @posts.by_month(field: :date).sum(:study_time)
+    @count_month_hour = hour(@count_month)
+    @count_week = @posts.by_week(field: :date, startday: :monday).sum(:study_time)
+    @count_week_hour = hour(@count_week)
     if @count_all > 0
       @count_daily_avg = @posts.after(Date.today-7).average(:study_time).round
-      @count_daily_avg_hour = sprintf("%.1f",@count_daily_avg/60.to_f)
+      @count_daily_avg_hour = hour(@count_week)
     end
   end
+
+  def hour(time)
+    sprintf("%.1f", time/60.to_f)
+  end
+
 end
